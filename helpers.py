ed90bc80dac27b9c226b73bd2e93481426bbb861
Tammie Chung <tammie.chung11@gmail.com>
2020-05-21 15:08:21 -0400

    add and test display_board

--- helpers.py
+++ helpers.py
@@ -1,9 +1,14 @@
 def clear_output():
   print('\n' * 100)
 
-# Step 1: Write a function that can print out a board. Set up your board as a list, where each index 1-9 corresponds with a number on a number pad, so you get a 3 by 3 board representation.
+# Step 1: Write a function that can print out a board. Set up your board as a list, where each index 1-9 corresponds with a number on a number pad, so you get a 3 by 3 board representation. The first item in the list is a throwaway.
 def display_board(board):
-  pass
+  print(board[1] + '|' + board[2] + '|' + board[3])
+  print('-----')
+  print(board[4] + '|' + board[5] + '|' + board[6])
+  print('-----')
+  print(board[7] + '|' + board[8] + '|' + board[9])
+
 
 # Step 2: Write a function that can take in a player input and assign their marker as 'X' or 'O'. Think about using while loops to continually ask until you get a correct answer.
 def player_input():
